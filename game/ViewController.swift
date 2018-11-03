dedd366d8bf4b320d44a09ad65da1781bce23be9
上原尚大 <utkhr@ueharanaodainomacbook-puro.local>
2018-11-03 20:54:38 +0900

    Initial Commit

---
+++ game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by 上原尚大 on 2018/11/03.
+//  Copyright © 2018年 上原尚大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
